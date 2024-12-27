1c577b93c62cc9203316b349b53293aa7bc9584d
jimmy-hsin <r09945014@ntu.edu.tw>
2024-12-27 14:17:00 +0800

    Update Heap, performance efficient.cpp

--- c++/2593. Find Score of an Array After Marking All Elements/Heap, performance efficient.cpp
+++ c++/2593. Find Score of an Array After Marking All Elements/Heap, performance efficient.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     long long findScore(vector<int>& nums) { 
         int n=nums.size();
-        vector<pair<int,int>> pq;
+        //由於heap裡的元素我們只要使用一次就丟了，沒有要重新push回去，因此使用vector作為容器再對她做排序的計算量會比較小
+        vector<pair<int,int>> pq(n);
         for(int i=0;i<n;i++)
-            pq.push_back({nums[i],i});
+            pq[i]={nums[i],i};
         sort(pq.begin(),pq.end());
+        
         vector<bool> visited(n,0);
         long long sum=0;
         for(int i=0;i<n;i++){
