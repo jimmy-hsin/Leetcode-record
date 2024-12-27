63412ee6b4a31bdd75ac6e690df6b61eeb1ad1ba
jimmy-hsin <r09945014@ntu.edu.tw>
2024-12-27 14:56:17 +0800

    Update Sliding Window.cpp

--- c++/2779. Maximum Beauty of an Array After Applying Operation/Sliding Window.cpp
+++ c++/2779. Maximum Beauty of an Array After Applying Operation/Sliding Window.cpp
@@ -4,12 +4,12 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         int l=0,r=0;
-        k=2*k;
+        k*=2;//由於可以移動的範圍是nums[i]-k ~ nums[i]+k，整個區間的大小為2k
         int res=0;
         while(r<n){
-            while(r<n && (nums[r]-nums[l])<=k)
+            while(r<n && (nums[r]-nums[l])<=k)    //如果nums[r]相對於nums[l]還在範圍內，則可以擴張右區間的邊界
                 r++;
-            res=max(res,r-l);
+            res=max(res,r-l);    //超過的話，先統計一下目前的合規區間長度
             l++;
         }
         return res;
