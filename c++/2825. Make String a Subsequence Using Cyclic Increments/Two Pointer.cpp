5db48d53ef20f95868daf23157b9bd995ab5abc1
jimmy-hsin <r09945014@ntu.edu.tw>
2024-12-30 11:42:15 +0800

    Update Two Pointer.cpp

--- c++/2825. Make String a Subsequence Using Cyclic Increments/Two Pointer.cpp
+++ c++/2825. Make String a Subsequence Using Cyclic Increments/Two Pointer.cpp
@@ -4,12 +4,12 @@ public:
         int n=str1.size(), m=str2.size();
         int i=0,j=0;
         while(i<n && j<m){
-            int c1=str1[i]-'a', c2=str2[j]-'a';
-            if(c1==c2 || (c1+1)%26==c2)
-                i++, j++;
+            int c1=str1[i]-'a', c2=str2[j]-'a';    //取出兩者的字母的排序
+            if(c1==c2 || (c1+1)%26==c2)            //如果str2[j]可以自str1[i]轉換
+                i++, j++;                    //兩個都可以望下一步邁進
             else
-                i++;
+                i++;                            //否則換str1的下一個字母試試
         }
-        return j==m;
+        return j==m;  //看看str2的所有字母有沒有都湊到了
     }
 };
