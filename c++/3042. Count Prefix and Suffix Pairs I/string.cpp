e5aa9cf10a7633f95bd62d35b84f2b2a1a96724a
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-08 08:20:12 +0800

    Create string.cpp

---
+++ c++/3042. Count Prefix and Suffix Pairs I/string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int n=words.size();
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                count+=isValid(words[i],words[j]);
+            }
+        }
+        return count;
+    }
+    bool isValid(string str1, string str2){
+        int len1=str1.size(), len2=str2.size();
+        if(len1 > len2) return false;
+        if(str2.substr(0,len1)!=str1)
+            return false;
+        if(str2.substr(len2-len1, len1)!=str1)
+            return false;
+        return true;
+    }
+};
