9b7510244f2b441e6d8b8f2e4808f03cf10736e8
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-13 10:53:05 +0800

    Create Counting2.cpp

---
+++ c++/3223. Minimum Length of String After Operations/Counting2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> count(26,0);
+        for(char& c:s)
+            count[c-'a']++;
+        int sum=0;
+        for(int& i:count){
+            if(i==0)    continue;
+            sum+=(2-i%2);
+        } 
+        return sum;
+    }
+};
