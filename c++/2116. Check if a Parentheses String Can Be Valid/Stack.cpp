ef8fd4b78f97ab143cb9b67645e8a1f081ddf849
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-13 08:19:24 +0800

    Update Stack.cpp

--- c++/2116. Check if a Parentheses String Can Be Valid/Stack.cpp
+++ c++/2116. Check if a Parentheses String Can Be Valid/Stack.cpp
@@ -3,21 +3,21 @@ public:
     bool canBeValid(string s, string locked) { 
         if(s.size()%2)  return false;
       
-        stack<int> st, flex;
+        stack<int> fixed, flex;
         for(int i=0;i<s.size();i++){
             if(locked[i]=='0')  flex.push(i);
-            else if(s[i]=='(')   st.push(i);
+            else if(s[i]=='(')   fixed.push(i);
             else if(s[i]==')'){
-                if(!st.empty()) st.pop();
+                if(!fixed.empty()) fixed.pop();
                 else if(!flex.empty())  flex.pop();
                 else return false;
             }
         }
-        while(!st.empty() && !flex.empty() && st.top()<flex.top()){
-            st.pop();
+        while(!fixed.empty() && !flex.empty() && fixed.top()<flex.top()){
+            fixed.pop();
             flex.pop();
         }
         if(flex.size()%2)   return false;
-        return st.empty();
+        return fixed.empty();
     }
 };
