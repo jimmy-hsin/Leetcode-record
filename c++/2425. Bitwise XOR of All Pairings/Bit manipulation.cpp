943e976f2db472b7713e52739ac74764fd3c64ce
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-16 08:38:30 +0800

    Update Bit manipulation.cpp

--- c++/2425. Bitwise XOR of All Pairings/Bit manipulation.cpp
+++ c++/2425. Bitwise XOR of All Pairings/Bit manipulation.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         /*
-            這題邏輯其實很簡單，XOR有一個特性，對同樣的數做XOR會得到0，
+            這題邏輯其實很簡單，XOR有一個特性，對兩個同樣的數做XOR會得到0，
             所以如果nums1裡的數要能對結果產生影響，nums2的總長度必須是奇數個，
             否則nums1裡的元素產生的配對總數為偶數個的情況下，所XOR的結果都會為0，就不會對結果有影響
 
