b30810fc92f979023e308c1c111c3f87e93bd82f
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-17 08:46:21 +0800

    Update Bit Manipulation.cpp

--- c++/2683. Neighboring Bitwise XOR/Bit Manipulation.cpp
+++ c++/2683. Neighboring Bitwise XOR/Bit Manipulation.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        bool res=0;
+        /*
+            先嘗試理解題意: 我們有一個origin array，用來產生derived array，
+            透過題目的規則，我們可以發現到一件事，就是每個origin array裡的元素都會被拿來操作XOR operation 兩次
+            所以理論上得出的derived array 全部做完XOR 之後的答案應該為0，所以我們找出對derived通乘(XOR)之後，就知道答案了
+        */
+        int res=0;
         for(int& i:derived)
             res^=i;
         return !res;
