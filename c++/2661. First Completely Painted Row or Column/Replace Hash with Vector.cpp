c496053d1131e25243436b6d315f28c08aa2fee3
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-20 08:30:19 +0800

    Update and rename Hash Table.cpp to Replace Hash with Vector.cpp

--- c++/2661. First Completely Painted Row or Column/Replace Hash with Vector.cpp
+++ c++/2661. First Completely Painted Row or Column/Replace Hash with Vector.cpp
@@ -3,6 +3,7 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m=mat.size(), n=mat[0].size();
         int k=arr.size();
+        //我們先創建兩個vector，以便我們後續快速查找arr[i]在mat中的位置
         vector<int> numToRow(k+1);
         vector<int> numToCol(k+1);
         for(int i=0;i<m;i++){
@@ -11,14 +12,13 @@ public:
                 numToCol[mat[i][j]]=j;
             }
         }
+        //再來創建兩個vector, 他們可以統計每一排，每一列現在標記多少塊方塊了
         vector<int> row(m,0);
         vector<int> col(n,0);
         for(int i=0;i<arr.size();i++){
             int x=numToRow[arr[i]];
             int y=numToCol[arr[i]];
-            row[x]++;
-            col[y]++;
-            if(row[x]==n || col[y]==m)
+            if(++row[x]==n || ++col[y]==m)    //如果當前要填入的行(row)，或列(col) 填入這個方塊後就湊滿了的話，就直接回傳i值
                 return i;
         }
         return 0;
