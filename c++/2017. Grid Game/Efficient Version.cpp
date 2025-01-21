3812c09a0510f0adaf12f692d5875b4940095c1a
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-21 08:57:46 +0800

    Update Efficient Version.cpp

--- c++/2017. Grid Game/Efficient Version.cpp
+++ c++/2017. Grid Game/Efficient Version.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n=grid[0].size();
-        long long top_sum = accumulate(grid[0].begin(), grid[0].end(), 0LL);
-        long long bottom_sum = 0;
+        long long back = accumulate(grid[0].begin(), grid[0].end(), 0LL);
+        long long front = 0;
+        
         long long minRes=LLONG_MAX; 
         for(int i=0;i<n;i++){
-            top_sum-=grid[0][i];
-            long long maxVal=max(top_sum, bottom_sum);
-            bottom_sum+=grid[1][i];
+            back-=grid[0][i];
+            long long maxVal=max(back, front);
             minRes=min(minRes, maxVal);
+            front+=grid[1][i];     
         }
         return minRes;
     }
