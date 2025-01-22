3fc497f56eee728208e137464c26cfb7b233e905
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-22 09:23:17 +0800

    Update DP.cpp

--- c++/1014. Best Sightseeing Pair/DP.cpp
+++ c++/1014. Best Sightseeing Pair/DP.cpp
@@ -13,7 +13,7 @@ public:
 };
 
 /*
-  由於分數的計算是values[a] + values[b] + a - b，那我們先把它拆項成 (values[a]+b) + (values[b]-b)，
+  由於分數的計算是values[a] + values[b] + a - b，那我們先把它拆項成 (values[a]+a) + (values[b]-b)，
   我們可以用currMax來記錄先前最大的(values[a]+a)的值，後一項的b為我們當前掃描到的(i)，所以我們能求出當前的元素i 與前項配對的最大分數(用temp記起來)，
   然後我們再紀錄一路掃描以來的最大分數就解決問題了
 */
