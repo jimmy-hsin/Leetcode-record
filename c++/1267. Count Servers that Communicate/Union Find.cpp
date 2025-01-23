01dc437826ddce73f41163d22b975146ac15e926
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-23 08:37:15 +0800

    Update Union Find.cpp

--- c++/1267. Count Servers that Communicate/Union Find.cpp
+++ c++/1267. Count Servers that Communicate/Union Find.cpp
@@ -3,6 +3,7 @@ public:
     int countServers(vector<vector<int>>& grid) {
         int m=grid.size(), n=grid[0].size();
         int computers=0;
+        //我們設置兩個vector，用來統計每一行、每一列各有多少電腦，同時記錄現有電腦的總數
         vector<int> row(m,0);
         vector<int> col(n,0);
         for(int i=0;i<m;i++)
@@ -12,13 +13,13 @@ public:
                     row[i]++;
                     col[j]++;
                 }
-        
+        //然後我們再來找找看孤立電腦有多少個
         int lonely=0;
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
-                if(grid[i][j]==1 && row[i]==1 && col[j]==1)
+                if(grid[i][j]==1 && row[i]==1 && col[j]==1)    //孤立電腦就是他所在的那一行及那一列都只有他自己
                     lonely++;
         
-        return computers-lonely;
+        return computers-lonely;    //把所有電腦的總數減去孤立電腦的數量就是答案
     }
 };
