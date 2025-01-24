bc8c37a50b9c0c68aeb4e4514442abbe4967d059
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-24 09:40:05 +0800

    Update DFS.cpp

--- c++/802. Find Eventual Safe States/DFS.cpp
+++ c++/802. Find Eventual Safe States/DFS.cpp
@@ -13,6 +13,8 @@ public:
     bool safe(int node, vector<vector<int>>& graph, vector<int>& visited){
         if(visited[node]!=0)  //有兩個可能他會進到這裡來: 1. 已經完成最終標記(也就是dfs的最後兩行，把狀態設為2)，知道這個節點已經是safe了;   
             return visited[node]==2;                  //2. 他還在下面的for迴圈的處理中，還沒到最終標記步驟就回來了，那就代表這個節點在循環內
+        // visited[node] == 2: 已經確定是安全節點，不需要再次處理
+        // visited[node] == 1: 該節點正在處理中，遇到這個情況代表循環
 
         visited[node]=1; //先把它標記成處理中
         for(auto& nxNode:graph[node]){
