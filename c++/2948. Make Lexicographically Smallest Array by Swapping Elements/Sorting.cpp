5c172f7787c028d9217ccd5ac57018e5e58b83f7
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-25 22:58:18 +0800

    Update Sorting.cpp

--- c++/2948. Make Lexicographically Smallest Array by Swapping Elements/Sorting.cpp
+++ c++/2948. Make Lexicographically Smallest Array by Swapping Elements/Sorting.cpp
@@ -7,26 +7,28 @@ public:
             所以我們第二步要做的就是把組別內的index由前到後進行排列後，這樣就會是每個數字的最終位置
         */
         int n=nums.size();
+        //紀錄每個數字的原始位置
         vector<pair<int,int>> pos(n);//val, index;
         for(int i=0;i<n;i++)
             pos[i]={nums[i],i};
+        //然後對數字進行排序
         ranges::sort(pos);
         
-        vector<int> group;//紀錄每個數字該擺在哪裡
+        vector<int> group;//紀錄每個數字最終該擺在哪裡
         int pre=0;  //每個組別的斷點
         group.push_back(pos[0].second);
         for(int i=1;i<n;i++){
-            if(pos[i].first-pos[i-1].first>limit){
-                sort(group.begin()+pre, group.end());//整理每個組別的位置(由前到後排序)
-                pre=i;
+            if(pos[i].first-pos[i-1].first>limit){    //遇到斷點了
+                sort(group.begin()+pre, group.end());//整理前一個組別的index(由前到後排序)
+                pre=i;    //重設斷點
             }
-            group.push_back(pos[i].second);
+            group.push_back(pos[i].second); 
         }
 
-        sort(group.begin()+pre, group.end());
+        sort(group.begin()+pre, group.end());//整理最後一組的index
 
         for(int i=0;i<n;i++)
-            nums[group[i]]=pos[i].first;
+            nums[group[i]]=pos[i].first; 
         return nums;
     }
 };
