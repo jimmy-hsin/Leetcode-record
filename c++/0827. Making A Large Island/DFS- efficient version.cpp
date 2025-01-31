633db0204bb6ead0344f5917b230cf70231dad56
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-31 15:39:15 +0800

    Update DFS- efficient version.cpp

--- c++/0827. Making A Large Island/DFS- efficient version.cpp
+++ c++/0827. Making A Large Island/DFS- efficient version.cpp
@@ -3,32 +3,27 @@ public:
     vector<int> dir = {-1, 0, 1, 0, -1};
     int largestIsland(vector<vector<int>>& grid) {
         // 第一步，只填號
-        int n = grid.size();
-        int id= 2;  //ID從2開始
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(grid[i][j]==1)
-                    dfs(grid, n, i, j, id++);
+        int n=grid.size();
+        vector<int> islandSize={0,0};    
+        int zeroCnt=0;   
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){   
+                    int count=dfs(grid, i, j, islandSize.size(), n); 
+                    islandSize.push_back(count);    
+                }
+                else if(grid[i][j]==0)
+                    zeroCnt++;
             }
         }
-
-        // 第二步，統計每個島嶼的大小
-        int ans = 0;
-        vector<int> areas(id,0);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] <= 1) 
-                    continue;
-                else 
-                    areas[grid[i][j]]++;
-                ans = max(ans, areas[grid[i][j]]);
-            }
-        }
-        if(areas.size()<=3)    return areas.back() + 1;
+        if(zeroCnt<=1)    //如果0的總個數<=1，代表轉化後就沒有0了
+            return n*n;
+        if(islandSize.size()<=3)    return islandSize.back() + 1;    
         
         // 嘗試連接
-        //因為只有四個方向，所以用一個全域變數做操作會比一直新建區域變數來的快
-        vector<bool> visited(id, false);
+        //因為只有四個方向，所以用一個全域變數做操作 會比一直新建區域變數來的快
+        int ans = 0;
+        vector<bool> visited(islandSize.size(), false);
         int up, down, left, right, merge;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -38,21 +33,20 @@ public:
                 down = (i < n-1) ? grid[i+1][j] : 0;
                 left = (j > 0) ? grid[i][j-1] : 0;
                 right = (j < n-1) ? grid[i][j+1] : 0;
-                
                 visited[up] = true;
-                merge = 1 + areas[up];
+                merge = 1 + islandSize[up];
             //確認沒有重複計算
                 if (!visited[down]) {
                     visited[down] = true;
-                    merge += areas[down];
+                    merge += islandSize[down];
                 }
                 if (!visited[left]) {
                     visited[left] = true;
-                    merge += areas[left];
+                    merge += islandSize[left];
                 }
                 if (!visited[right]) {
                     visited[right] = true;
-                    merge += areas[right];
+                    merge += islandSize[right];
                 }
                 ans = max(ans, merge);
                 visited[up] = false;
@@ -61,15 +55,16 @@ public:
                 visited[right] = false;
             }
         }
-
         return ans;
     }
 
-    void dfs(vector<vector<int>>& grid, int n, int i, int j, int id) {
-        if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != 1) return; //出界，或不是陸地，或為已經探訪過的陸地
-
-        grid[i][j] = id;
-        for(int d=0;d<4;d++)
-            dfs(grid, n, i+dir[d], j+dir[d+1], id);
+    int dfs(vector<vector<int>>& grid, int x, int y, int ID, int n){
+        if(x<0 || y<0 || x>=n || y>=n || grid[x][y]!=1)    
+            return 0;
+        int count=1;
+        grid[x][y]=ID;        
+        for(int i=0;i<4;i++)
+            count+=dfs(grid,x+dir[i],y+dir[i+1], ID, n);    
+        return count;
     }
 };
