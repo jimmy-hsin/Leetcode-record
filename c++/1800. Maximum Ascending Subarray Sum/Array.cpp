e17d135cb0a3e0fef4620da0895f649940678be8
jimmy-hsin <r09945014@ntu.edu.tw>
2025-02-04 17:10:05 +0800

    Update Array.cpp

--- c++/1800. Maximum Ascending Subarray Sum/Array.cpp
+++ c++/1800. Maximum Ascending Subarray Sum/Array.cpp
@@ -4,10 +4,10 @@ public:
         int res=0;
         int tempRes=nums[0];
         for(int i=1;i<nums.size();i++){
-            if(nums[i]>nums[i-1])
+            if(nums[i]>nums[i-1]) //如果比前一個數小，就繼續往後加
                 tempRes+=nums[i];
-            else{
-                res=max(res,tempRes);
+            else{                //不然就重開一條subarray,自己當開頭
+                res=max(res,tempRes);    //紀錄最大的前一個subarray的和
                 tempRes=nums[i];
             }   
         }
