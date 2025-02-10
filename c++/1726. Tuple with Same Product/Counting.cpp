3628e50a152a3bfd013459f67c867966c926c447
jimmy-hsin <r09945014@ntu.edu.tw>
2025-02-10 11:20:57 +0800

    Create Counting.cpp

---
+++ c++/1726. Tuple with Same Product/Counting.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        vector<int> product;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++)
+                product.push_back(nums[i]*nums[j]);
+        }
+        ranges::sort(product);
+        int count=0, same=1;
+        product.push_back(0);
+        for(int i=1;i<product.size();i++){
+            if(product[i]==product[i-1])    same++;
+            else{
+                if(same>1) count+=same*(same-1)*4;
+                same=1;
+            }
+        }
+        return count;
+        
+    }
+};
